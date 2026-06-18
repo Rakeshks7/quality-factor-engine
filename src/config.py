e7b27f59ff3ee0ee0667ec2bd9a6d2d994a37d5a
Rakesh K <rakeshdraft4@gmail.com>
2026-06-18 12:31:58 +0530

    Add files via upload

---
+++ src/config.py
@@ -0,0 +1,10 @@
+INITIAL_CAPITAL = 2_000_000  
+LONG_EXPOSURE = 1_000_000
+SHORT_EXPOSURE = -1_000_000
+
+QUALITY_WEIGHTS = {
+    'roe': 0.5,             
+    'debt_to_equity': -0.5  
+}
+
+FF_URL = "https://mba.tuck.dartmouth.edu/pages/faculty/ken.french/ftp/F-F_Research_Data_5_Factors_2x3_CSV.zip"
